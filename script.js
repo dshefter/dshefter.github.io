60a36e5c862509d5f85c7d46dff938c1da79fac1
Deena Shefter <deenashefter@Deenas-MBP.home>
2020-05-10 18:40:01 -0400

    fixed skills bars and changed color layout

---
+++ script.js
@@ -0,0 +1,310 @@
+function showAll() {
+    document.getElementById('IndependentProjectPlanning').style.display = 'block';
+    document.getElementById('ScientificWriting').style.display = 'block';
+    document.getElementById('PublicSpeaking').style.display = 'block';
+    document.getElementById('Presentation').style.display = 'block';
+    document.getElementById('Numpy').style.display = 'block';
+    document.getElementById('Pandas').style.display = 'block';
+    document.getElementById('Sci-kitlearn').style.display = 'block';
+    document.getElementById('Matplotlib').style.display = 'block';
+    document.getElementById('Plotly').style.display = 'block';
+    document.getElementById('Seaborn').style.display = 'block';
+    document.getElementById('Catmaid').style.display = 'block';
+    document.getElementById('NeuTu').style.display = 'block';
+    document.getElementById('Cellculture').style.display = 'block';
+    document.getElementById('IndirectELISA').style.display = 'block';
+    document.getElementById('Polymerasechainreaction').style.display = 'block';
+    document.getElementById('Flowcytometry').style.display = 'block';
+    document.getElementById('Gelelectrophoresis').style.display = 'block';
+    document.getElementById('Neuralnetworks').style.display = 'block';
+    document.getElementById('Chi-squared').style.display = 'block';
+    document.getElementById('Regression').style.display = 'block';
+    document.getElementById('Linearalgebra').style.display = 'block';
+    document.getElementById('Multivariablecalculus').style.display = 'block';
+    document.getElementById('Probability').style.display = 'block';
+    document.getElementById('HTML').style.display = 'block';
+    document.getElementById('CSS').style.display = 'block';
+    document.getElementById('Javascript').style.display = 'block';
+    document.getElementById('Java').style.display = 'block';
+    document.getElementById('Python').style.display = 'block';
+    document.getElementById('R').style.display = 'block';
+    document.getElementById('ImageJ').style.display = 'block';
+ 
+    document.getElementById("skillrows").scrollIntoView({behavior: "smooth"});
+}
+function hideAll() {
+    document.getElementById('IndependentProjectPlanning').style.display = 'none';
+    document.getElementById('ScientificWriting').style.display = 'none';
+    document.getElementById('PublicSpeaking').style.display = 'none';
+    document.getElementById('Presentation').style.display = 'none';
+    document.getElementById('Numpy').style.display = 'none';
+    document.getElementById('Pandas').style.display = 'none';
+    document.getElementById('Sci-kitlearn').style.display = 'none';
+    document.getElementById('Matplotlib').style.display = 'none';
+    document.getElementById('Plotly').style.display = 'none';
+    document.getElementById('Seaborn').style.display = 'none';
+    document.getElementById('Catmaid').style.display = 'none';
+    document.getElementById('NeuTu').style.display = 'none';
+    document.getElementById('Cellculture').style.display = 'none';
+    document.getElementById('IndirectELISA').style.display = 'none';
+    document.getElementById('Polymerasechainreaction').style.display = 'none';
+    document.getElementById('Flowcytometry').style.display = 'none';
+    document.getElementById('Gelelectrophoresis').style.display = 'none';
+    document.getElementById('Neuralnetworks').style.display = 'none';
+    document.getElementById('Chi-squared').style.display = 'none';
+    document.getElementById('Regression').style.display = 'none';
+    document.getElementById('Linearalgebra').style.display = 'none';
+    document.getElementById('Multivariablecalculus').style.display = 'none';
+    document.getElementById('Probability').style.display = 'none';
+    document.getElementById('HTML').style.display = 'none';
+    document.getElementById('CSS').style.display = 'none';
+    document.getElementById('Javascript').style.display = 'none';
+    document.getElementById('Java').style.display = 'none';
+    document.getElementById('Python').style.display = 'none';
+    document.getElementById('R').style.display = 'none';
+    document.getElementById('ImageJ').style.display = 'none';
+}
+function showResearch() {
+
+    document.getElementById('IndependentProjectPlanning').style.display = 'block';
+    document.getElementById('ScientificWriting').style.display = 'block';
+    document.getElementById('PublicSpeaking').style.display = 'block';
+    document.getElementById('Presentation').style.display = 'block';
+    document.getElementById('Numpy').style.display = 'none';
+    document.getElementById('Pandas').style.display = 'none';
+    document.getElementById('Sci-kitlearn').style.display = 'none';
+    document.getElementById('Matplotlib').style.display = 'none';
+    document.getElementById('Plotly').style.display = 'none';
+    document.getElementById('Seaborn').style.display = 'none';
+    document.getElementById('Catmaid').style.display = 'none';
+    document.getElementById('NeuTu').style.display = 'none';
+    document.getElementById('Cellculture').style.display = 'none';
+    document.getElementById('IndirectELISA').style.display = 'none';
+    document.getElementById('Polymerasechainreaction').style.display = 'none';
+    document.getElementById('Flowcytometry').style.display = 'none';
+    document.getElementById('Gelelectrophoresis').style.display = 'none';
+    document.getElementById('Neuralnetworks').style.display = 'none';
+    document.getElementById('Chi-squared').style.display = 'none';
+    document.getElementById('Regression').style.display = 'none';
+    document.getElementById('Linearalgebra').style.display = 'none';
+    document.getElementById('Multivariablecalculus').style.display = 'none';
+    document.getElementById('Probability').style.display = 'none';
+    document.getElementById('HTML').style.display = 'none';
+    document.getElementById('CSS').style.display = 'none';
+    document.getElementById('Javascript').style.display = 'none';
+    document.getElementById('Java').style.display = 'none';
+    document.getElementById('Python').style.display = 'none';
+    document.getElementById('R').style.display = 'none';
+    document.getElementById('ImageJ').style.display = 'none';
+
+    document.getElementById("skillrows").scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+}
+function showComp() {
+    document.getElementById('IndependentProjectPlanning').style.display = 'none';
+    document.getElementById('ScientificWriting').style.display = 'none';
+    document.getElementById('PublicSpeaking').style.display = 'none';
+    document.getElementById('Presentation').style.display = 'none';
+    document.getElementById('Numpy').style.display = 'block';
+    document.getElementById('Pandas').style.display = 'block';
+    document.getElementById('Sci-kitlearn').style.display = 'block';
+    document.getElementById('Matplotlib').style.display = 'block';
+    document.getElementById('Plotly').style.display = 'block';
+    document.getElementById('Seaborn').style.display = 'block';
+    document.getElementById('Catmaid').style.display = 'block';
+    document.getElementById('NeuTu').style.display = 'block';
+    document.getElementById('Cellculture').style.display = 'none';
+    document.getElementById('IndirectELISA').style.display = 'none';
+    document.getElementById('Polymerasechainreaction').style.display = 'none';
+    document.getElementById('Flowcytometry').style.display = 'none';
+    document.getElementById('Gelelectrophoresis').style.display = 'none';
+    document.getElementById('Neuralnetworks').style.display = 'none';
+    document.getElementById('Chi-squared').style.display = 'none';
+    document.getElementById('Regression').style.display = 'none';
+    document.getElementById('Linearalgebra').style.display = 'none';
+    document.getElementById('Multivariablecalculus').style.display = 'none';
+    document.getElementById('Probability').style.display = 'none';
+    document.getElementById('HTML').style.display = 'none';
+    document.getElementById('CSS').style.display = 'none';
+    document.getElementById('Javascript').style.display = 'none';
+    document.getElementById('Java').style.display = 'none';
+    document.getElementById('Python').style.display = 'none';
+    document.getElementById('R').style.display = 'none';
+    document.getElementById('ImageJ').style.display = 'none';
+
+    document.getElementById("skills-bars").scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+}
+
+function showDataAnal() {
+    document.getElementById('IndependentProjectPlanning').style.display = 'none';
+    document.getElementById('ScientificWriting').style.display = 'none';
+    document.getElementById('PublicSpeaking').style.display = 'none';
+    document.getElementById('Presentation').style.display = 'none';
+    document.getElementById('Numpy').style.display = 'block';
+    document.getElementById('Pandas').style.display = 'block';
+    document.getElementById('Sci-kitlearn').style.display = 'block';
+    document.getElementById('Matplotlib').style.display = 'block';
+    document.getElementById('Plotly').style.display = 'block';
+    document.getElementById('Seaborn').style.display = 'block';
+    document.getElementById('Catmaid').style.display = 'none';
+    document.getElementById('NeuTu').style.display = 'none';
+    document.getElementById('Cellculture').style.display = 'none';
+    document.getElementById('IndirectELISA').style.display = 'none';
+    document.getElementById('Polymerasechainreaction').style.display = 'none';
+    document.getElementById('Flowcytometry').style.display = 'none';
+    document.getElementById('Gelelectrophoresis').style.display = 'none';
+    document.getElementById('Neuralnetworks').style.display = 'none';
+    document.getElementById('Chi-squared').style.display = 'none';
+    document.getElementById('Regression').style.display = 'none';
+    document.getElementById('Linearalgebra').style.display = 'none';
+    document.getElementById('Multivariablecalculus').style.display = 'none';
+    document.getElementById('Probability').style.display = 'block';
+    document.getElementById('HTML').style.display = 'none';
+    document.getElementById('CSS').style.display = 'none';
+    document.getElementById('Javascript').style.display = 'none';
+    document.getElementById('Java').style.display = 'none';
+    document.getElementById('Python').style.display = 'none';
+    document.getElementById('R').style.display = 'none';
+    document.getElementById('ImageJ').style.display = 'none';
+
+    document.getElementById("skills-bars").scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+}
+
+function showExper() {
+    document.getElementById('IndependentProjectPlanning').style.display = 'none';
+    document.getElementById('ScientificWriting').style.display = 'none';
+    document.getElementById('PublicSpeaking').style.display = 'none';
+    document.getElementById('Presentation').style.display = 'none';
+    document.getElementById('Numpy').style.display = 'none';
+    document.getElementById('Pandas').style.display = 'none';
+    document.getElementById('Sci-kitlearn').style.display = 'none';
+    document.getElementById('Matplotlib').style.display = 'none';
+    document.getElementById('Plotly').style.display = 'none';
+    document.getElementById('Seaborn').style.display = 'none';
+    document.getElementById('Catmaid').style.display = 'none';
+    document.getElementById('NeuTu').style.display = 'none';
+    document.getElementById('Cellculture').style.display = 'block';
+    document.getElementById('IndirectELISA').style.display = 'block';
+    document.getElementById('Polymerasechainreaction').style.display = 'block';
+    document.getElementById('Flowcytometry').style.display = 'block';
+    document.getElementById('Gelelectrophoresis').style.display = 'block';
+    document.getElementById('Neuralnetworks').style.display = 'none';
+    document.getElementById('Chi-squared').style.display = 'none';
+    document.getElementById('Regression').style.display = 'none';
+    document.getElementById('Linearalgebra').style.display = 'none';
+    document.getElementById('Multivariablecalculus').style.display = 'none';
+    document.getElementById('Probability').style.display = 'none';
+    document.getElementById('HTML').style.display = 'none';
+    document.getElementById('CSS').style.display = 'none';
+    document.getElementById('Javascript').style.display = 'none';
+    document.getElementById('Java').style.display = 'none';
+    document.getElementById('Python').style.display = 'none';
+    document.getElementById('R').style.display = 'none';
+    document.getElementById('ImageJ').style.display = 'none';
+
+    document.getElementById("skills-bars").scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+}
+
+function showQuant() {
+    document.getElementById('IndependentProjectPlanning').style.display = 'none';
+    document.getElementById('ScientificWriting').style.display = 'none';
+    document.getElementById('PublicSpeaking').style.display = 'none';
+    document.getElementById('Presentation').style.display = 'none';
+    document.getElementById('Numpy').style.display = 'none';
+    document.getElementById('Pandas').style.display = 'none';
+    document.getElementById('Sci-kitlearn').style.display = 'none';
+    document.getElementById('Matplotlib').style.display = 'none';
+    document.getElementById('Plotly').style.display = 'none';
+    document.getElementById('Seaborn').style.display = 'none';
+    document.getElementById('Catmaid').style.display = 'none';
+    document.getElementById('NeuTu').style.display = 'none';
+    document.getElementById('Cellculture').style.display = 'none';
+    document.getElementById('IndirectELISA').style.display = 'none';
+    document.getElementById('Polymerasechainreaction').style.display = 'none';
+    document.getElementById('Flowcytometry').style.display = 'none';
+    document.getElementById('Gelelectrophoresis').style.display = 'none';
+    document.getElementById('Neuralnetworks').style.display = 'block';
+    document.getElementById('Chi-squared').style.display = 'block';
+    document.getElementById('Regression').style.display = 'block';
+    document.getElementById('Linearalgebra').style.display = 'block';
+    document.getElementById('Multivariablecalculus').style.display = 'block';
+    document.getElementById('Probability').style.display = 'block';
+    document.getElementById('HTML').style.display = 'none';
+    document.getElementById('CSS').style.display = 'none';
+    document.getElementById('Javascript').style.display = 'none';
+    document.getElementById('Java').style.display = 'none';
+    document.getElementById('Python').style.display = 'none';
+    document.getElementById('R').style.display = 'none';
+    document.getElementById('ImageJ').style.display = 'none';
+
+    document.getElementById("skills-bars").scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+}
+
+function showWebDev() {
+    document.getElementById('IndependentProjectPlanning').style.display = 'none';
+    document.getElementById('ScientificWriting').style.display = 'none';
+    document.getElementById('PublicSpeaking').style.display = 'none';
+    document.getElementById('Presentation').style.display = 'none';
+    document.getElementById('Numpy').style.display = 'none';
+    document.getElementById('Pandas').style.display = 'none';
+    document.getElementById('Sci-kitlearn').style.display = 'none';
+    document.getElementById('Matplotlib').style.display = 'none';
+    document.getElementById('Plotly').style.display = 'none';
+    document.getElementById('Seaborn').style.display = 'none';
+    document.getElementById('Catmaid').style.display = 'none';
+    document.getElementById('NeuTu').style.display = 'none';
+    document.getElementById('Cellculture').style.display = 'none';
+    document.getElementById('IndirectELISA').style.display = 'none';
+    document.getElementById('Polymerasechainreaction').style.display = 'none';
+    document.getElementById('Flowcytometry').style.display = 'none';
+    document.getElementById('Gelelectrophoresis').style.display = 'none';
+    document.getElementById('Neuralnetworks').style.display = 'none';
+    document.getElementById('Chi-squared').style.display = 'none';
+    document.getElementById('Regression').style.display = 'none';
+    document.getElementById('Linearalgebra').style.display = 'none';
+    document.getElementById('Multivariablecalculus').style.display = 'none';
+    document.getElementById('Probability').style.display = 'none';
+    document.getElementById('HTML').style.display = 'block';
+    document.getElementById('CSS').style.display = 'block';
+    document.getElementById('Javascript').style.display = 'block';
+    document.getElementById('Java').style.display = 'none';
+    document.getElementById('Python').style.display = 'none';
+    document.getElementById('R').style.display = 'none';
+    document.getElementById('ImageJ').style.display = 'none';
+
+    document.getElementById("skills-bars").scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+}
+
+function showLang() {
+    document.getElementById('IndependentProjectPlanning').style.display = 'none';
+    document.getElementById('ScientificWriting').style.display = 'none';
+    document.getElementById('PublicSpeaking').style.display = 'none';
+    document.getElementById('Presentation').style.display = 'none';
+    document.getElementById('Numpy').style.display = 'none';
+    document.getElementById('Pandas').style.display = 'none';
+    document.getElementById('Sci-kitlearn').style.display = 'none';
+    document.getElementById('Matplotlib').style.display = 'none';
+    document.getElementById('Plotly').style.display = 'none';
+    document.getElementById('Seaborn').style.display = 'none';
+    document.getElementById('Catmaid').style.display = 'none';
+    document.getElementById('NeuTu').style.display = 'none';
+    document.getElementById('Cellculture').style.display = 'none';
+    document.getElementById('IndirectELISA').style.display = 'none';
+    document.getElementById('Polymerasechainreaction').style.display = 'none';
+    document.getElementById('Flowcytometry').style.display = 'none';
+    document.getElementById('Gelelectrophoresis').style.display = 'none';
+    document.getElementById('Neuralnetworks').style.display = 'none';
+    document.getElementById('Chi-squared').style.display = 'none';
+    document.getElementById('Regression').style.display = 'none';
+    document.getElementById('Linearalgebra').style.display = 'none';
+    document.getElementById('Multivariablecalculus').style.display = 'none';
+    document.getElementById('Probability').style.display = 'none';
+    document.getElementById('HTML').style.display = 'block';
+    document.getElementById('CSS').style.display = 'block';
+    document.getElementById('Javascript').style.display = 'block';
+    document.getElementById('Java').style.display = 'block';
+    document.getElementById('Python').style.display = 'block';
+    document.getElementById('R').style.display = 'block';
+    document.getElementById('ImageJ').style.display = 'block';
+
+    document.getElementById("skills-bars").scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+}
